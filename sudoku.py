de933e744cf4228a0235a240a71e189f81b53ef3
derrickandry <47150184+derrickandry@users.noreply.github.com>
2020-04-05 02:50:31 +0700

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -13,7 +13,7 @@ board = [
         ]
 
 def printBoard(b):
-    for x in range(9):    
+    for x in range(9):
         if x%3 == 0 and x!=0:
             print("---------------------")
         for y in range(9):
@@ -21,29 +21,44 @@ def printBoard(b):
                 print('| ', end= "")
             print(board[x][y], end=" ")
         print()
-        
+    print()
     return
-
+printBoard(board)
 def possible(x,y,n):
     #check row
-    if board[x].count(n) > 1:
-        return False
+    for m in range(9):
+        if board[x][m] == n:
+            return False
     #check column
     for m in range(9):
-        if board[m][y] == n and m != x:
+        if board[m][y] == n:
             return False
-    
+
     #check block
-    blockX = x % 3
-    blockY = y % 3
-    if board[blockX][blockY] == n:
-        return False
-    if board[blockX + 2][blockY + 2] == n:
-        return False
-    if board[blockX][blockY + 2] == n:
-        return False
-    if board[blockX + 2][blockY] == n:
-        return False
-    
+    x0 = (x//3)*3
+    y0 = (y//3)*3
+    for i in range(0,3):
+        for j in range(0,3):
+            if board[x0+i][y0+j] == n:
+                return False
+
     return True
+def solve():
+    global board
+    for x in range(9):
+        for y in range(9):
+            if board[x][y] == 0:
+                for n in range(1,10):
+                    if possible(x,y,n):
+                        board[x][y] = n
+                        solve()
+                        board[x][y] = 0
+                return
+
+    printBoard(board)
+    input()
+
+
+solve()
+
 
